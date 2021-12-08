5da137e66aa4ae40e09a0a95dfbc552043968b45
HanJiHun <49916333+hahahohohun@users.noreply.github.com>
2021-12-08 16:48:48 +0900

    const 연습

--- cpp1/cpp/cpp/main.cpp
+++ cpp1/cpp/cpp/main.cpp
@@ -1,23 +1,20 @@
 using namespace std;
 #include <iostream>
 #include <string.h>
-struct  tag_Friend
-{
-	char Name[10];
-	int Age;
-	double Height;
-};
 
 int main()
 {
-	
-	short sArr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int* pI = (int*)sArr;
-	int iData = *((short*)(pI + 2));
-	printf("%d\n", iData); //답은 5.
+	int a = 1;
+	int b = 20;
+	//const 포인터. 가리키는 값을 상수화.
+	const int* pConstInt = &a;
+	//*pConstInt = 100; //불가능.
+	pConstInt = &b;
 
-	char cArr[2] = { 1,1 };
-	short* pS = (short*)cArr;
-	iData = *pS;
-	printf("%d", iData); //답은 257/
+	//포인터 const.
+	int* const pIntConst = &a;
+	*pIntConst = 200;
+	//pIntConst = &b; //불가능.
+
 }
+
