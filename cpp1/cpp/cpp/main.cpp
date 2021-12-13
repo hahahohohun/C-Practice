054256ce76212bd66039d2e24b85c2b25066927f
HanJiHun <49916333+hahahohohun@users.noreply.github.com>
2021-12-13 16:27:33 +0900

    포인터연습
    문자열(1)

--- cpp1/cpp/cpp/main.cpp
+++ cpp1/cpp/cpp/main.cpp
@@ -4,17 +4,13 @@ using namespace std;
 
 int main()
 {
-	int a = 1;
-	int b = 20;
-	//const 포인터. 가리키는 값을 상수화.
-	const int* pConstInt = &a;
-	//*pConstInt = 100; //불가능.
-	pConstInt = &b;
+	wchar_t szWchar[] = L"abcdef";
+	szWchar[1] = 'z';
+	//배열의 두번째 값을 'b' -> 'z'로 바꾼다.
 
-	//포인터 const.
-	int* const pIntConst = &a;
-	*pIntConst = 200;
-	//pIntConst = &b; //불가능.
-
+	const wchar_t* pChar = L"abcdef";
+	//wchar_t* pChar = L"abcdef"; //컴파일에러 : 수정이 불가한 코드영역이라 const키워드가 필요함.
+	//pChar[1] = 'z'; // 컴파일에러: 힙메모리에 접근하여 값을 수정하는 wchar_t szWchar[1]와 다르게 
+	//"abcdef"이라고 작성된 코드(ROM)의 시작주소를 받는 포인터변수에서는 변경이 불가하다.
 }
 
