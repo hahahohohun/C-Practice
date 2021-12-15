c84e68eb29a9103ffbd7bdd4c08dea85d3266d50
HanJiHun <49916333+hahahohohun@users.noreply.github.com>
2021-12-15 23:54:32 +0900

    문자열 이어 붙이기

--- cpp1/cpp/cpp/main.cpp
+++ cpp1/cpp/cpp/main.cpp
@@ -1,16 +1,52 @@
 using namespace std;
 #include <iostream>
 #include <string.h>
+#include <cassert>
+
+unsigned int GetLength(const wchar_t* pDest)
+{
+	int nIdx = 0;
+	while (true)
+	{
+		if (pDest[nIdx] == '\0')
+			break;
+		nIdx++;
+	}
+
+	return nIdx;
+}
+
+void StrCat(wchar_t* pDest, unsigned int nBufferSize, const wchar_t* source)
+{
+	int nDesLength = GetLength(pDest);
+	int nSourceLength = GetLength(source);
+	//주어진 버퍼 길이보다 길면.
+	if (nBufferSize < nDesLength + nSourceLength + 1) //+1 : null 문자 공간까지 계산
+	{
+		assert(nullptr);
+	}
+
+	// 문자열 이어 붙이기.
+	// 1. Dest문자열의 끝을 확인(문자열이 이어 붙을 시작 위치)
+
+	// 2. 반복적으로 Src 문자열을 Dest끝 위치에 복사하기
+	// 3. Src 문자열의 끝을 만나면 반복 종료
+	for (int i = 0; i < nSourceLength + 1; i++)
+	{
+		if (pDest[nDesLength + i] == '\0')
+		{
+			pDest[nDesLength + i] = source[i];
+		}
+	}
+}
 
 int main()
 {
-	wchar_t szWchar[] = L"abcdef";
-	szWchar[1] = 'z';
-	//배열의 두번째 값을 'b' -> 'z'로 바꾼다.
+	wchar_t szString[100] = L"abc";
+	//wcscat_s(szString, 100, L"def");
+	StrCat(szString, 100, L"def");
 
-	const wchar_t* pChar = L"abcdef";
-	//wchar_t* pChar = L"abcdef"; //컴파일에러 : 수정이 불가한 코드영역이라 const키워드가 필요함.
-	//pChar[1] = 'z'; // 컴파일에러: 힙메모리에 접근하여 값을 수정하는 wchar_t szWchar[1]와 다르게 
-	//"abcdef"이라고 작성된 코드(ROM)의 시작주소를 받는 포인터변수에서는 변경이 불가하다.
+	printf("%d", GetLength(szString));
 }
+
 
