4df042386aa80c79c36baaac4a22915c6a10a583
HanJiHun <49916333+hahahohohun@users.noreply.github.com>
2021-12-21 11:54:14 +0900

    C 배열기반 리스트 연습

--- cpp1/cpp/cpp/main.cpp
+++ cpp1/cpp/cpp/main.cpp
@@ -1,84 +1,80 @@
-using namespace std;
+//using namespace std;
 #include <iostream>
-#include <string.h>
-#include <cassert>
+#include <wchar.h>
+#define INF 100000
 
-unsigned int GetLength(const wchar_t* pDest)
+int arr[INF];
+int count = 0;
+
+void AddBack(int n)
 {
-	int nIdx = 0;
+	arr[count] = n;
+	count++;
+}
+
+void AddFirst(int n)
+{
+	for (int i = count; i >= 1; i--)
+	{
+		arr[i] = arr[i - 1];
+	}
+	arr[0] = n;
+	count++;
+}
+
+void AddAtIndex(int nIdx, int nData)
+{
+	for (int i = count; i >= nIdx; i--)
+	{
+		int nTempData = arr[i];
+		arr[i + 1] = nTempData;
+	}
+	arr[nIdx] = nData;
+}
+
+void RemoveFirst()
+{
+	printf("삭제된 data %d\n", arr[0]);
+	for (int i = 1; i < count; i++)
+	{
+		arr[i - 1] = arr[i];
+	}
+	arr[count - 1] = 0;
+	count--;
+}
+
+void RemoveBack()
+{
+	printf("삭제된 data %d\n", arr[count - 1]);
+	arr[count - 1] = 0;
+	count--;
+}
+
+void Show()
+{
+	int i = 0;
 	while (true)
 	{
-		if (pDest[nIdx] == '\0')
+		if (arr[i] == 0)
+		{
 			break;
-		nIdx++;
-	}
-
-	return nIdx;
-}
-
-void StrCat(wchar_t* pDest, unsigned int nBufferSize, const wchar_t* source)
-{
-	int nDesLength = GetLength(pDest);
-	int nSourceLength = GetLength(source);
-	//주어진 버퍼 길이보다 길면.
-	if (nBufferSize < nDesLength + nSourceLength + 1) //+1 : null 문자 공간까지 계산
-	{
-		assert(nullptr);
-	}
-
-	// 문자열 이어 붙이기.
-	// 1. Dest문자열의 끝을 확인(문자열이 이어 붙을 시작 위치)
-
-	// 2. 반복적으로 Src 문자열을 Dest끝 위치에 복사하기
-	// 3. Src 문자열의 끝을 만나면 반복 종료
-	for (int i = 0; i < nSourceLength + 1; i++)
-	{
-		if (pDest[nDesLength + i] == '\0')
-		{
-			pDest[nDesLength + i] = source[i];
 		}
-	}
-}
-
-//같을 경우 :0.
-//왼쪽이 클경우 1.
-//오른쪽이 클경우 -1.
-int StrCmp(const wchar_t* pLeft, const wchar_t* pRight)
-{
-	int nLeftLength = GetLength(pLeft);
-	int nRightLength = GetLength(pRight);
-	int nLoop = nLeftLength > nRightLength ? nRightLength : nLeftLength;
-	int nReturn = 0;
-
-	if (nLeftLength > nRightLength)
-	{
-		nReturn = 1;
-	}
-	else if (nLeftLength < nRightLength)
-	{
-		nReturn = -1;
+		printf("%d\n", arr[i]);
+		i++;
 	}
 
-	for (int i = 0; i < nLoop; i++)
-	{
-		if (pLeft[i] < pRight[i])
-		{
-			return -1;
-		}
-		else if (pLeft[i] > pRight[i])
-		{
-			return 1;
-		}
-	}
-
-	return nReturn;
 }
 
 int main()
 {
-	int n = StrCmp(L"gbg", L"abb");
-
-	printf("%d", n);
+	//AddFirst(1);
+	//AddFirst(2);
+	//AddFirst(3);
+	AddBack(1);
+	AddBack(2);
+	AddBack(3);
+	AddBack(4);
+	RemoveFirst();
+	RemoveBack();
+	Show();
 }
-
-
